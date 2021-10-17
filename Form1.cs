46ff4a4acd323b1a21b6d44e06ca9ec0b3b4f433
sravaniRavulakollu <87975506+sravaniRavulakollu@users.noreply.github.com>
2021-10-17 22:33:15 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace window_form1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.Black;
+            label1.Text = "Now you can understand Event Driven Programming in c#";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.Indigo;
+            label1.Text = " Naresh ";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Welcome to GUI Demo";
+            label1.ForeColor = Color.Red;
+        }
+    }
+}
